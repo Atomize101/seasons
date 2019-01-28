066037b7d2c729bf240871527f43032b2714761c
Christopher Patrick <cpatrick@nabancard.com>
2019-01-28 17:01:24 -0500

    Project Complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 class App extends React.Component {
     
@@ -13,14 +14,22 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent() {
         if(this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if(!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />
         }
-        return <div>Still Loading...</div>
+        return <Spinner  />;
+    }
+
+    render() {
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
